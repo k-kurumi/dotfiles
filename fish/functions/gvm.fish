77474aa5ef65a3a4618e5523e4e40897af0bb4de
k-kurumi <optpia.kurumi@gmail.com>
2016-11-11 13:15:10 +0900

    remove gvm

--- fish/functions/gvm.fish
+++ fish/functions/gvm.fish
@@ -1,3 +1,5 @@
-function gvm
-  bass source ~/.gvm/scripts/gvm ';' gvm $argv
-end
+# golangはバージョン切り替えなしで最新版を使うことにする
+# 複数必要なときは、バイナリを落として GOROOT を書き換える？
+#function gvm
+#  bass source ~/.gvm/scripts/gvm ';' gvm $argv
+#end
