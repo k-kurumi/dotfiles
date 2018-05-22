fab2b5324088fc04338eb5c85dc2d5f9642d1928
k-kurumi <optpia.kurumi@gmail.com>
2018-05-22 13:35:08 +0900

    update hyper.js plugins

--- .hyper.js
+++ .hyper.js
@@ -115,7 +115,11 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyperterm-material',
+  ],
+
+
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
