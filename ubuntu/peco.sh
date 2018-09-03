2e36ccff56baf2026f49fd75e06ee725de391c10
Ubuntu <ubuntu@ip-172-26-15-181.ap-northeast-1.compute.internal>
2018-09-03 04:26:58 +0000

    Fix peco.sh

--- ubuntu/peco.sh
+++ ubuntu/peco.sh
@@ -4,7 +4,7 @@
 
 pushd /tmp
   rm -rf peco*
-  wget https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_arm64.tar.gz
-  tar zxvf peco_linux_arm64.tar.gz
-  sudo mv peco_linux_arm64/peco/peco /usr/local/bin/
+  wget https://github.com/peco/peco/releases/download/v0.5.3/peco_linux_amd64.tar.gz
+  tar zxvf peco_linux_amd64.tar.gz
+  sudo mv peco_linux_amd64/peco /usr/local/bin/
 popd
