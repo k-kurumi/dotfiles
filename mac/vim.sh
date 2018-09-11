43aa94910c3595167b05960cd66faed7ebed0a99
k-kurumi <optpia.kurumi@gmail.com>
2018-09-11 14:46:44 +0900

    fix macvim

--- mac/vim.sh
+++ mac/vim.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+#
+# macvim: gvim相当
 
 set -e
 set -x
 
 # reinstallではエラーになることがあるため
 brew uninstall vim
+brew uninstall macvim
+
+brew cleanup vim
+brew cleanup macvim
 
 brew install lua
 
-# デフォルトで +ruby +python が付く
 brew install vim --with-luajit
+brew install macvim --with-lua
