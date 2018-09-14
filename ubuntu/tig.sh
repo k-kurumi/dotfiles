4aa15a5c9141b7552788416dda9376a6c5b79fbd
k-kurumi <optpia.kurumi@gmail.com>
2018-09-14 15:34:45 +0900

    add tig installer

---
+++ ubuntu/tig.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+version=2.4.1
+
+sudo apt-get update
+sudo apt-get install -y \
+  libncursesw5 \
+  libncursesw5-dev
+
+pushd /tmp
+
+  rm -rf tig-*
+  wget https://github.com/jonas/tig/releases/download/tig-${version}/tig-${version}.tar.gz
+  tar zxvf tig-${version}.tar.gz
+
+  pushd tig-${version}
+    make configure
+    LDLIBS=-lncursesw CFLAGS=-I/usr/include/ncursesw ./configure --prefix=/usr/local
+    make
+    sudo make install
+  popd
+
+popd
