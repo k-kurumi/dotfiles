67cf3c763faea9d73b883695ba3135ddca6f6af0
Ubuntu <ubuntu@ip-172-26-15-181.ap-northeast-1.compute.internal>
2018-10-11 01:43:57 +0000

    add direnv installer

---
+++ ubuntu/direnv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# install direnv
+
+version=v2.17.0
+
+pushd /tmp
+  rm -rf direnv*
+  wget https://github.com/direnv/direnv/releases/download/${version}/direnv.linux-amd64 -O direnv
+  chmod +x direnv
+  sudo mv direnv /usr/local/bin/
+popd
