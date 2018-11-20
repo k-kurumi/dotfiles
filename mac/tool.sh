f4789bb76b13b64eb4127df44847f5b60eef49d0
k-kurumi <optpia.kurumi@gmail.com>
2018-11-20 22:48:10 +0900

    update mac tools

--- mac/tool.sh
+++ mac/tool.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#
-# macvim: gvim相当
-
-set -e
-set -x
 
 brew install lua
 brew install vim --with-luajit
-
 # xcode(gui)が必要らしい(xcode(cli)だけではエラー)
 # brew install macvim --with-lua
+
+brew install zsh
+brew install tmux
+brew install tree
+brew install mosh
+brew install peco
+
+brew cleanup
