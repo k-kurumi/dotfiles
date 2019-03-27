ec35b96ae8159693e7eb5ad4976b256e1e8d05d0
k-kurumi <optpia.kurumi@gmail.com>
2019-03-27 10:13:56 +0900

    fix vim installer

--- ubuntu/vim.sh
+++ ubuntu/vim.sh
@@ -1,81 +1,73 @@
 #!/bin/bash
 #
 # install vim
-# https://vim-jp.org/docs/build_linux.html
 
 set -e
 set -x
 
 sudo apt-get update
 
-if [[ -z ${DISPLAY} ]]; then
+sudo apt-get install -y \
+  ncurses-dev \
+  python-dev \
+  python3-dev \
+  ruby-dev \
+  lua5.2 \
+  lua5.2-dev \
+  luajit \
+  git
 
-  # CUIのみ
-  sudo apt-get install -y \
-    ncurses-dev \
-    python-dev \
-    python3-dev \
-    ruby-dev \
-    lua5.2 \
-    lua5.2-dev \
-    luajit \
-    git
-
-  pushd /tmp
-    rm -rf vim
-    git clone git@github.com:vim/vim.git
-    pushd vim
-      ./configure \
-        --with-features=huge \
-        --enable-multibyte \
-        --enable-pythoninterp=yes \
-        --enable-python3interp=yes \
-        --enable-rubyinterp=yes \
-        --enable-luainterp=yes \
-        --enable-cscope \
-        --enable-gpm \
-        --enable-fail-if-missing
-      make
-      sudo make install
-    popd
+pushd /tmp
+  rm -rf vim
+  git clone git@github.com:vim/vim.git
+  pushd vim
+    ./configure \
+      --with-features=huge \
+      --enable-multibyte \
+      --enable-pythoninterp=yes \
+      --enable-python3interp=yes \
+      --enable-rubyinterp=yes \
+      --enable-luainterp=yes \
+      --enable-cscope \
+      --enable-gpm \
+      --enable-fail-if-missing
+    make
+    sudo make install
   popd
+popd
 
-else
+# gvimはクリップボード周りが使いづらいのでvscodeで十分
+# GUI持っているかの判定は [[ -z ${DISPLAY} ]] && echo cui || echo gui
+#
+# sudo apt-get install -y \
+#   ncurses-dev \
+#   python-dev \
+#   python3-dev \
+#   ruby-dev \
+#   lua5.2 \
+#   lua5.2-dev \
+#   luajit \
+#   libxmu-dev \
+#   libgtk-3-dev \
+#   libxpm-dev \
+#   git
 
-  # GUIあり
-  sudo apt-get install -y \
-    ncurses-dev \
-    python-dev \
-    python3-dev \
-    ruby-dev \
-    lua5.2 \
-    lua5.2-dev \
-    luajit \
-    libxmu-dev \
-    libgtk-3-dev \
-    libxpm-dev \
-    git
-
-  # libxmu-dev libgtk-3-dev libxpm-dev
-
-  pushd /tmp
-    rm -rf vim
-    git clone git@github.com:vim/vim.git
-    pushd vim
-      ./configure \
-        --with-features=huge \
-        --enable-multibyte \
-        --enable-pythoninterp=yes \
-        --enable-python3interp=yes \
-        --enable-rubyinterp=yes \
-        --enable-luainterp=yes \
-        --enable-cscope \
-        --enable-gpm \
-        --enable-gui=gtk3 \
-        --enable-fail-if-missing
-      make
-      sudo make install
-    popd
-  popd
-
-fi
+# pushd /tmp
+#   rm -rf vim
+#   git clone git@github.com:vim/vim.git
+#   pushd vim
+#     ./configure \
+#       --with-features=huge \
+#       --enable-multibyte \
+#       --enable-pythoninterp=yes \
+#       --enable-python3interp=yes \
+#       --enable-rubyinterp=yes \
+#       --enable-luainterp=yes \
+#       --enable-cscope \
+#       --enable-gpm \
+#       --enable-gui=gtk3 \
+#       --enable-fail-if-missing
+#     make
+#     sudo make install
+#   popd
+# popd
