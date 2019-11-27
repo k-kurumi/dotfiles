1cc8939540739d5072cb645a790067cadfb75ac4
k-kurumi <optpia.kurumi@gmail.com>
2019-11-27 10:34:58 +0900

    Change fzf installer

--- ubuntu/fzf.sh
+++ ubuntu/fzf.sh
@@ -2,11 +2,5 @@
 #
 # install fzf
 
-version=0.18.0
-
-pushd /tmp
-  rm -rf fzf*
-  wget https://github.com/junegunn/fzf-bin/releases/download/${version}/fzf-${version}-linux_amd64.tgz
-  tar zxvf fzf-${version}-linux_amd64.tgz
-  sudo mv fzf /usr/local/bin/
-popd
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
