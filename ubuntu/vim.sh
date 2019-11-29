169d1f8dd0eee9530ca305a094996f2b04d27ded
k-kurumi <optpia.kurumi@gmail.com>
2019-11-29 14:29:40 +0900

    vim clipboard enabled

--- ubuntu/vim.sh
+++ ubuntu/vim.sh
@@ -30,44 +30,9 @@ pushd /tmp
       --enable-luainterp=yes \
       --enable-cscope \
       --enable-gpm \
+      --enable-gui=auto \
       --enable-fail-if-missing
     make
     sudo make install
   popd
 popd
-
-# gvimはクリップボード周りが使いづらいのでvscodeで十分
-# GUI持っているかの判定は [[ -z ${DISPLAY} ]] && echo cui || echo gui
-#
-# sudo apt-get install -y \
-#   ncurses-dev \
-#   python-dev \
-#   python3-dev \
-#   ruby-dev \
-#   lua5.2 \
-#   lua5.2-dev \
-#   luajit \
-#   libxmu-dev \
-#   libgtk-3-dev \
-#   libxpm-dev \
-#   git
-
-# pushd /tmp
-#   rm -rf vim
-#   git clone git@github.com:vim/vim.git
-#   pushd vim
-#     ./configure \
-#       --with-features=huge \
-#       --enable-multibyte \
-#       --enable-pythoninterp=yes \
-#       --enable-python3interp=yes \
-#       --enable-rubyinterp=yes \
-#       --enable-luainterp=yes \
-#       --enable-cscope \
-#       --enable-gpm \
-#       --enable-gui=gtk3 \
-#       --enable-fail-if-missing
-#     make
-#     sudo make install
-#   popd
-# popd
