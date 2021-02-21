2d0349650e31bb4ef2078a087293fb2f9c946d24
k-kurumi <optpia.kurumi@gmail.com>
2021-02-21 23:56:56 +0900

    fix wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,11 +9,33 @@ return {
 
   -- font
   font = wezterm.font("Cica", {bold=true, italic=false}),
-  font_size = 14.0,
+  font_size = 18.0,
 
   -- https://github.com/mbadolato/iTerm2-Color-Schemes#screenshots
-  color_scheme = "Molokai",
+  color_scheme = "Espresso",
 
-  -- デフォルトのショートカット
+  -- 背景透過 1.0が不透過
+  window_background_opacity = 0.9,
+
+  -- キーバインド変更
   -- https://wezfurlong.org/wezterm/config/keys.html#default-shortcut--key-binding-assignments
+  -- SUPER == command
+  keys = {
+    -- cmd+h 左のタブを表示する
+    {key="h", mods="SUPER", action=wezterm.action{ActivateTabRelative=-1}},
+    -- cmd+l 右のタブを表示する
+    {key="l", mods="SUPER", action=wezterm.action{ActivateTabRelative=1}},
+
+    -- cmd+n タブを開く
+    {key="n", mods="SUPER", action=wezterm.action{SpawnTab="CurrentPaneDomain"}},
+
+    -- cmd+H タブを左に移動する
+    {key="h", mods="SHIFT|SUPER", action=wezterm.action{MoveTabRelative=-1}},
+    -- cmd+L タブを右に移動する
+    {key="l", mods="SHIFT|SUPER", action=wezterm.action{MoveTabRelative=1}},
+
+    -- 基本US配列なのか key="+",mods="SUPER" では効かない
+    -- cmd++ フォントを大きくする
+    {key="+", mods="SHIFT|SUPER", action="IncreaseFontSize"},
+  },
 }
