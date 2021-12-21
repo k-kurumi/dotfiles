59cdf5f11216da7dc4fad2bf8b83f86dd29ec76c
k-kurumi <optpia.kurumi@gmail.com>
2021-12-21 10:33:34 +0900

    fix vimrc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 -- paq package manager https://github.com/savq/paq-nvim
 local install_path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+  vim.fn.system({'git', 'clone', 'https://github.com/savq/paq-nvim.git', install_path})
 end
 vim.cmd [[packadd paq-nvim]]
 local paq = require('paq').paq
