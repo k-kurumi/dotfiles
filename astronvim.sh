0b063a3b48f2fc5d39bec7585ea447e89739a436
k-kurumi <optpia.kurumi@gmail.com>
2023-04-29 16:33:36 +0900

    add astronvim.sh

---
+++ astronvim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# install astronvim and config
+#
+
+git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone git@github.com:k-kurumi/astronvim_config.git ~/.config/nvim/lua/user
