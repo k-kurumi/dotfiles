d6c141c72193b9fe108a75642d6af31ea6e5037d
k-kurumi <optpia.kurumi@gmail.com>
2023-09-06 11:50:33 +0900

    astronvim

--- astronvim.sh
+++ astronvim.sh
@@ -4,9 +4,12 @@
 #
 
 if [[ -d ~/.config/nvim/.git ]]; then
-  pushd ~/.config/nvim
+  pushd ~/.config/nvim || exit 1
+    # デフォルトのhlsearch周りが気に入らないので修正した部分をstash
+    git stash
     git pull
-  popd
+    git stash pop
+  popd || exit 1
 else
   mv ~/.config/nvim ~/.config/nvim.bak
   mv ~/.local/share/nvim ~/.local/share/nvim.bak
@@ -16,9 +19,9 @@ else
 fi
 
 if [[ -d ~/.config/nvim/lua/user/.git ]]; then
-  pushd ~/.config/nvim/lua/user
+  pushd ~/.config/nvim/lua/user || exit 1
     git pull
-  popd
+  popd || exit 1
 else
   mv ~/.config/nvim/lua/user /tmp
   git clone git@github.com:k-kurumi/astronvim_config.git ~/.config/nvim/lua/user
