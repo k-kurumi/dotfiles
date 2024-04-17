0a80aa3aad501069fd34fc7fc8f2b0e2b554720a
k-kurumi <optpia.kurumi@gmail.com>
2024-04-17 21:56:32 +0900

    devcontainer

--- devcontainer/init.sh
+++ devcontainer/init.sh
@@ -2,8 +2,14 @@
 #
 # devcontainerの初期設定スクリプト
 #
-# 複数PCで利用するためfeaturesでhomebrewはインストールしない
-# (runArgsでlinux/amd64指定してもホストがarm64だとコンテナビルドに失敗するため)
+# features: homebrew はarm64でビルドできない
+# runArgsでlinux/amd64指定してもビルド不可
+# 起動後に手動でインストールすることは可能
+#
+# ターミナルのシェルをzshに変更するfeatureはrunArgsのlinux/amd64指定でビルド不可
+# homebrewは諦めて、aptでインストールする方がよさそう
+#
+# このスクリプトは、aptでインストールできない必須のツールをインストールする
 #
 
 if [ -z "${REMOTE_CONTAINERS}" ]; then
@@ -11,24 +17,6 @@ if [ -z "${REMOTE_CONTAINERS}" ]; then
   exit 0
 fi
 
-# homebrewはlinux(arm64)に対応していない
-if [[ $(uname -m) == "x86_64" ]]; then
-  type brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-  grep linuxbrew ~/.profile || echo 'export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH' >> ~/.profile
-else
-  echo "homebrew unsupported architecture: $(uname -m)"
-  exit 1
-fi
-
-#
-# 以降はhomebrewインストール済み
-#
-
-# type brew && brew install vim tmux fzf jq ripgrep ghq sheldon gh tig lazygit direnv
-
-# # evalはzsh起動して実行する必要あり
-# type fzf > /dev/null && cat <<'EOL'
-# ## Set up fzf key bindings and fuzzy completion
-# ## evalはzsh起動して実行する必要あり
-# eval "$(fzf --zsh)"
-# EOL
+# sheldon
+curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+  | bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
