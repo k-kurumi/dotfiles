4a7d4b244bb35a8d5f27504842d07b155667bd95
k-kurumi <optpia.kurumi@gmail.com>
2024-04-17 14:24:25 +0900

    devcontainer

--- devcontainer/init.sh
+++ devcontainer/init.sh
@@ -1,19 +1,31 @@
-#!/usr/bin/zsh
+#!/bin/bash
 #
 # devcontainerの初期設定スクリプト
 # eval fzf の関係でシェバンはzshを指定している
 #
 
-if [ -z ${REMOTE_CONTAINERS} ]; then
+if [ -z "${REMOTE_CONTAINERS}" ]; then
   echo "Not in a remote container, skipping..."
   exit 0
 fi
 
-if type brew; then
-
-  type brew && brew install vim tmux fzf jq ripgrep ghq sheldon gh tig lazygit
-
-  # Set up fzf key bindings and fuzzy completion
-  eval "$(fzf --zsh)"
-
+# homebrewはlinux(arm64)に対応していない
+if [[ $(uname -m) == "x86_64" ]]; then
+  type brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "homebrew unsupported architecture: $(uname -m)"
+  exit 1
 fi
+
+#
+# 以降はhomebrewインストール済み
+#
+
+brew install vim tmux fzf jq ripgrep ghq sheldon gh tig lazygit direnv
+
+# evalはzsh起動して実行する必要あり
+type fzf > /dev/null && cat <<'EOL'
+## Set up fzf key bindings and fuzzy completion
+## evalはzsh起動して実行する必要あり
+eval "$(fzf --zsh)"
+EOL
