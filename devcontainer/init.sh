0696c88c7dd7b52bd2435515cdc5f90e52776906
k-kurumi <optpia.kurumi@gmail.com>
2024-04-17 17:00:03 +0900

    devcontainer

--- devcontainer/init.sh
+++ devcontainer/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # devcontainerの初期設定スクリプト
-# eval fzf の関係でシェバンはzshを指定している
+#
+# 複数PCで利用するためfeaturesでhomebrewはインストールしない
+# (runArgsでlinux/amd64指定してもホストがarm64だとコンテナビルドに失敗するため)
 #
 
 if [ -z "${REMOTE_CONTAINERS}" ]; then
@@ -11,7 +13,8 @@ fi
 
 # homebrewはlinux(arm64)に対応していない
 if [[ $(uname -m) == "x86_64" ]]; then
-  type brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  type brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  grep linuxbrew ~/.profile || echo 'export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH' >> ~/.profile
 else
   echo "homebrew unsupported architecture: $(uname -m)"
   exit 1
@@ -21,11 +24,11 @@ fi
 # 以降はhomebrewインストール済み
 #
 
-brew install vim tmux fzf jq ripgrep ghq sheldon gh tig lazygit direnv
+# type brew && brew install vim tmux fzf jq ripgrep ghq sheldon gh tig lazygit direnv
 
-# evalはzsh起動して実行する必要あり
-type fzf > /dev/null && cat <<'EOL'
-## Set up fzf key bindings and fuzzy completion
-## evalはzsh起動して実行する必要あり
-eval "$(fzf --zsh)"
-EOL
+# # evalはzsh起動して実行する必要あり
+# type fzf > /dev/null && cat <<'EOL'
+# ## Set up fzf key bindings and fuzzy completion
+# ## evalはzsh起動して実行する必要あり
+# eval "$(fzf --zsh)"
+# EOL
