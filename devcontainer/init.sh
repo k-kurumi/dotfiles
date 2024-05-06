61504468fbdf21a87a7e5d6b8bd1d49934588a5e
k-kurumi <optpia.kurumi@gmail.com>
2024-05-06 19:06:47 +0900

    devcontainer

--- devcontainer/init.sh
+++ devcontainer/init.sh
@@ -17,7 +17,7 @@ if [ -z "${REMOTE_CONTAINERS}" ]; then
   exit 0
 fi
 
-type apt-get && sudo apt-get update && sudo apt-get install -y tig vim ripgrep tmux direnv git-crypt neofetch
+type apt-get && sudo apt-get update && sudo apt-get install -y tig vim ripgrep tmux direnv git-crypt neofetch file
 
 # fzfをaptでインストールすると補完周りが効かないため
 test -d ~/.fzf || git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
