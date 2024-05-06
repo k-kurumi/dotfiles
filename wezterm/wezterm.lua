993a7bea0f3a4faa6092650281780659ee953056
k-kurumi <optpia.kurumi@gmail.com>
2024-05-06 10:42:16 +0900

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,23 +9,41 @@ return {
 
   -- font
   font = wezterm.font("UDEV Gothic NF", {bold=true, italic=false}),
-  font_size = 18.0,
+  font_size = 16.0,
 
   -- https://github.com/mbadolato/iTerm2-Color-Schemes#screenshots
   -- color_scheme = "Gruvbox Dark",
   -- color_scheme = "Hybrid",
   -- color_scheme = "Monokai (terminal.sexy)",
-  -- color_scheme = "Neon (terminal.sexy)",
+  color_scheme = "Neon (terminal.sexy)",
   -- color_scheme = "PencilDark",
-  color_scheme = "Sakura",
+  -- color_scheme = "Sakura",
   -- color_scheme = "tokyonight",
   -- color_scheme = "Urple",
   -- color_scheme = "WildCherry",
 
+  background = {
+    {
+      source = {
+        -- ~は使えない
+        File = os.getenv("HOME") .. "/dotfiles/images/stained-glass.jpg",
+      },
+      -- sourceは改行しても固定
+      attachment = "Fixed",
+      -- sourceの明度
+      hsb =  { brightness = 0.005 },
+      -- 背景透過 0に近いほど透過する
+      opacity = 1.0,
+      horizontal_offset = 0,
+      vertical_offset = 0,
+    },
+  },
+
   -- 背景透過 1.0が不透過
-  window_background_opacity = 0.9,
+  window_background_opacity = 1.0,
 
   -- キーバインド変更
+  -- FIXME SUPERはpoposのウインドウ切り替えと被るので変更したい
   -- https://wezfurlong.org/wezterm/config/keys.html#default-shortcut--key-binding-assignments
   -- SUPER == command
   -- https://wezfurlong.org/wezterm/config/lua/keyassignment/index.html
