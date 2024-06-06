1101c58963c0d0125c7788fbfba1f6d4a234ebb4
k-kurumi <optpia.kurumi@gmail.com>
2024-06-06 20:06:55 +0900

    init.sh

--- init.sh
+++ init.sh
@@ -75,7 +75,8 @@ git config --global core.pager 'less -x4'
 git config --global commit.verbose true
 
 # コミット時のエディタ
-type code && git config --global core.editor 'code --wait' || git config --global core.editor vim
+# type code && git config --global core.editor 'code --wait' || git config --global core.editor vim
+type lvim && git config --global core.editor lvim || git config --global core.editor vim
 
 # globalなgitignoreを追加
 git config --global core.excludesfile '~/.gitignore_global'
