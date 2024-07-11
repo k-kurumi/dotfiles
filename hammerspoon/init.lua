a766a756265e8222ff0e8b9083f1ab7916865cc3
k-kurumi <optpia.kurumi@gmail.com>
2024-07-11 15:28:13 +0900

    切り替えは英かなに任せる

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -18,39 +18,3 @@ toggleApp("Google Chrome", "option", "c")
 toggleApp("Obsidian", "option", "o")
 toggleApp("1Password", "option", "1")
 toggleApp("KeePassXC", "option", "k")
-
--- 英かなアプリの代わりに左右のcmdで英かな切り替え
--- https://zenn.dev/ytk6565/articles/hammerspoon-switch-input-source
-local map = hs.keycodes.map
-local keyDown = hs.eventtap.event.types.keyDown
-local flagsChanged = hs.eventtap.event.types.flagsChanged
-local keyStroke = hs.eventtap.keyStroke
-
-local isCmdAsModifier = false
-
-local function switchInputSourceEvent(event)
-    local eventType = event:getType()
-    local keyCode = event:getKeyCode()
-    local flags = event:getFlags()
-    local isCmd = flags['cmd']
-
-    if eventType == keyDown then
-        if isCmd then
-            isCmdAsModifier = true
-        end
-    elseif eventType == flagsChanged then
-        if not isCmd then
-            if isCmdAsModifier == false then
-                if keyCode == map['cmd'] then
-                    keyStroke({}, 0x66, 0) -- 英数キー
-                elseif keyCode == map['rightcmd'] then
-                    keyStroke({}, 0x68, 0) -- かなキー
-                end
-            end
-            isCmdAsModifier = false
-        end
-    end
-end
-
-eventTap = hs.eventtap.new({keyDown, flagsChanged}, switchInputSourceEvent)
-eventTap:start()
