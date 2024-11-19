a448c91edb1bcea2eba7f366eafcb30769be3204
k-kurumi <optpia.kurumi@gmail.com>
2024-11-19 14:30:39 +0900

    hyper

--- .hyper.js
+++ .hyper.js
@@ -9,7 +9,7 @@ module.exports = {
     updateChannel: "stable",
 
     // default font size in pixels for all tabs
-    fontSize: 18,
+    fontSize: 20,
 
     // font family with optional fallbacks
     // fontFamily: "Cica",
